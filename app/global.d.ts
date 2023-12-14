d7e5dd76d4cff4507e5d1c7524e437c174294639
Yusuke Wada <yusuke@kamawada.com>
2023-12-15 08:26:52 +0900

    update

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,11 +1,3 @@
-import 'hono'
-
-declare module 'hono' {
-  interface ContextRenderer {
-    (children: React.ReactElement): Response | Promise<Response>
-  }
-}
-
 declare module '@hono/react-renderer' {
   interface Props {
     title: string
