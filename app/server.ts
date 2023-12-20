cd5f649770c53330541ae8ecdddc900a6f617290
Yusuke Wada <yusuke@kamawada.com>
2023-12-20 11:05:57 +0900

    use `init`

--- app/server.ts
+++ app/server.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'sonik/server'
 import { serveStatic } from 'hono/cloudflare-pages'
-import { Hono } from 'hono'
-
-const app = new Hono()
-app.use('/static/*', serveStatic())
 
 export default createApp({
-  app
+  init: (app) => {
+    app.use('/static/*', serveStatic())
+  }
 })
